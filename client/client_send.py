6f4ae5ce70e27f6e55f43eb209f69537b49ca3f9
IdelIshbaev <41520293+IdelIshbaev@users.noreply.github.com>
2020-10-04 01:50:12 +0300

    send FILE/receive FILE

--- client/client_send.py
+++ client/client_send.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import os
+
 """ it will be read from sys.argv[0]
 0 Initialize. 
 1 File create.
@@ -28,53 +29,49 @@ import os
 """
 
 if sys.argv[1] != 3:
-	# client send
-	client_socket = socket.socket(socket.AF_INET,
-	                              socket.SOCK_STREAM)
-	server_addr = ("10.0.15.50", 22)
-	client_socket.connect(server_addr)
-	command = sys.argv[1]
-	try:
-		additional_arg = sys.argv[2]
-	except:
-		additional_arg = '' 
-	temp = command
-	command = str(temp) + '_'
-	command += additional_arg	
-	client_socket.send(bytes(command, "utf8"))
-	print('Sent!')
+    # client send
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_addr = ("10.0.15.50", 22)
+    client_socket.connect(server_addr)
+    command = sys.argv[1]
+    try:
+        additional_arg = sys.argv[2]
+    except:
+        additional_arg = ""
+    temp = command
+    command = str(temp) + "_"
+    command += additional_arg
+    client_socket.send(bytes(command, "utf8"))
+    print("Sent!")
 
-	# close
-	client_socket.close()
+    # close
+    client_socket.close()
 else:
-	# client send 3
-	client_socket = socket.socket(socket.AF_INET,
-	                              socket.SOCK_STREAM)
-	server_addr = ("10.0.15.50", 22)
-	client_socket.connect(server_addr)
-	command = sys.argv[1]	
-	temp = command
-	command = str(temp) + '_'
-	client_socket.send(bytes(command, "utf8"))
-	print('Sent!')
-	client_socket.close()
+    # client send 3
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_addr = ("10.0.15.50", 22)
+    client_socket.connect(server_addr)
+    command = sys.argv[1]
+    temp = command
+    command = str(temp) + "_"
+    client_socket.send(bytes(command, "utf8"))
+    print("Sent!")
+    client_socket.close()
 
+    # client send file
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_addr = ("10.0.15.50", 22)
+    client_socket.connect(server_addr)
+    filename = sys.argv[2]
+    filesize = os.path.getsize(filename)
+    client_socket.send("{}{}{}".format(filename, sep, filesize).encode())
 
-	# client send file
-	client_socket = socket.socket(socket.AF_INET,
-	                              socket.SOCK_STREAM)
-	server_addr = ("10.0.15.50", 22)
-	client_socket.connect(server_addr)
-	filename = sys.argv[2]
-	filesize = os.path.getsize(filename)
-	client_socket.send("{}{}{}".format(filename, sep, filesize).encode())
+    print("Sent!")
 
-	print('Sent!')
+    # close
+    client_socket.close()
 
-	# close
-	client_socket.close()
-
-''' what to receive 
+""" what to receive 
 0 text - available size
 1 ok
 2 FILE  
@@ -89,11 +86,10 @@ else:
 10 ok
 11 ok
     need confirmation if there are files in the directory
-'''
+"""
 
 # client receive response
-server_socket = socket.socket(socket.AF_INET,
-                              socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_addr = ("10.0.15.50", 22)
 server_socket.bind(server_addr)
 server_socket.listen(1000)
@@ -103,21 +99,19 @@ server_socket.close()
 
 msg = client_socket.recv(1024)
 if msg != 2:
-    print ("File downloaded succesfully" )
+    print("File downloaded succesfully")
 elif msg == 11:
     # recieve confirmation
-    server_socket = socket.socket(socket.AF_INET,
-                                  socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_addr = ("10.0.15.50", 22)
     server_socket.bind(server_addr)
     server_socket.listen(1000)
     (client_socket, addr) = server_socket.accept()
-    print (msg)
+    print(msg)
     a = input
     server_socket.close()
-    #send confirmation
-    client_socket = socket.socket(socket.AF_INET,
-                                  socket.SOCK_STREAM)
+    # send confirmation
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_addr = ("10.0.15.50", 22)
     client_socket.connect(server_addr)
     command = sys.argv[1]
