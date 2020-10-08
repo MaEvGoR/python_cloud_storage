8f185f35f45b583dc8def9a27fd8eb8d40d0341d
IdelIshbaev <41520293+IdelIshbaev@users.noreply.github.com>
2020-10-09 01:55:43 +0300

    temp

--- client/cloudstore_client.py
+++
@@ -1,8 +0,0 @@
-# client service python script
-#!/usr/bin/python3
-
-import time
-
-while True:
-    print "This is a test python file!"
-    time.sleep(2)
--- namenode/cloudstore_namenode.py
+++
@@ -1,8 +0,0 @@
-# namenode service python script
-#!/usr/bin/python3
-
-import time
-
-while True:
-    print "This is a test python file!"
-    time.sleep(2)
